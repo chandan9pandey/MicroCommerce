896758fc481df5d4077a46a3ca860caaaf460ad1
chandan9pandey <chandan9pandey@gmail.com>
2026-09-18 00:08:23 +0530

    Service Discovery added using Eureka

--- inventory-service/src/main/java/com/chandan9pandey/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/chandan9pandey/inventoryservice/service/InventoryService.java
@@ -1,5 +1,6 @@
 package com.chandan9pandey.inventoryservice.service;
 
+import com.chandan9pandey.inventoryservice.dto.InventoryResponse;
 import com.chandan9pandey.inventoryservice.model.Inventory;
 import com.chandan9pandey.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +17,8 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
-    }
+	public List<InventoryResponse> isInStock(List<String> skuCode) {
+		return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory -> InventoryResponse.builder()
+				.skuCode(inventory.getSkuCode()).isInStock(inventory.getQuantity() > 0).build()).toList();
+	}
 }
