69554ff7f7f7c50fc7b0efec5b61b5e95cb42c2e
chandan9pandey <chandan9pandey@gmail.com>
2026-09-22 23:39:16 +0530

    notification-service implemented for asynchronous notification using Apache Kafka

--- order-service/src/main/java/com/chandan9pandey/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/chandan9pandey/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.chandan9pandey.orderservice.service;
 import com.chandan9pandey.orderservice.dto.InventoryResponse;
 import com.chandan9pandey.orderservice.dto.OrderLineItemsDto;
 import com.chandan9pandey.orderservice.dto.OrderRequest;
+import com.chandan9pandey.orderservice.event.OrderPlacedEvent;
 import com.chandan9pandey.orderservice.model.Order;
 import com.chandan9pandey.orderservice.model.OrderLineItems;
 import com.chandan9pandey.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -50,6 +53,8 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            // Send order to the kafka topic
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order Placed Successfully!";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
